162bb0fccc7a0273c76312a2cd1dc9f0fc0b9f86
davidetoscano129 <toscano.davidew@gmail.com>
2025-05-14 10:51:39 +0200

    Snack 3 completed

--- src/type.ts
+++ src/type.ts
@@ -7,3 +7,14 @@ type Dipendente = {
     anniDiServizio: number[];
 }
 
+type Developer = Dipendente & {
+    livelloEsperienza: "junior" | "Mid" | "Senior";
+    linguaggi?: string[];
+    certificazioni: string[]
+}
+
+type ProjectManager = Dipendente & {
+    teamSize: number | null;
+    budgetGestito: number;
+    stakeholderPrincipali: string[]
+}
